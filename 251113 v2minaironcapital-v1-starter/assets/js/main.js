6e9c88b1d67b7b16c232dc09aa615fe401f4bb2d
FerranBurgaya <118138810+FerranBurgaya@users.noreply.github.com>
2025-12-04 23:58:59 +0100

    Delete 251113 v2minaironcapital-v1-starter directory

--- 251113 v2minaironcapital-v1-starter/assets/js/main.js
+++
@@ -1,2 +0,0 @@
-
-(function(){try{if(!localStorage.getItem('cookieok')){document.getElementById('cookiebar').style.display='flex';document.getElementById('cookie-ok').onclick=function(){localStorage.setItem('cookieok','1');document.getElementById('cookiebar').remove();}}}catch(e){}})();
